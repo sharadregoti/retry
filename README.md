0b856a21be8d30d917d89341c436c296deb73af4
sharadregoti <sharadregoti15@gmail.com>
2023-09-18 16:35:20 +0530

    tested some more changed

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # retry
+
+## Installation
+`curl https://i.jpillora.com/sharadregoti/retry@v0.1.0! | bash`
--- app.sh
+++ app.sh
@@ -1,4 +1,5 @@
 echo "starting server..."
 sleep 2s
 echo "connection timed out"
+echo "connection refused"
 false
